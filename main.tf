25b59f3a6bcf16954522b17db25b9d3bcc1c53b9
silllvio <silllvio@hotmail.com>
2023-04-06 18:47:30 +0100

    sync

--- main.tf
+++ main.tf
@@ -1,1 +1,5 @@
- 
+resource "oci_objectstorage_bucket" "this" {
+  compartment_id = var.compartment_id
+  name           = "test_bucket_name"
+  namespace      = var.bucket_namespace
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
+variable "oci_tenancy_ocid" {}
+variable "oci_user_ocid" {}
+variable "oci_private_key_path" {}
+variable "oci_ssh_authorized_key" {}
+variable "oci_finger_print" {}
 
+variable "compartment_id" {
+  type = string
+}
+
+variable "bucket_namespace" {
+  type = string
+
+}
+
+variable "oci_location" {
+  type    = string
+  default = "us-ashburn-1"
+
+}
